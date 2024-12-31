56ad1ea2877cd2440c9d3882e010b8e697267889
jaylyn <jaylyng02@icloud.com>
2024-12-31 09:30:03 -0500

    Added port scanning and device information

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, jsonify
 from monitor.devices import scan_network
+from monitor.port_scan import scan_ports
 
 app = Flask(__name__)
 
 @app.route('/api/devices', methods=['GET'])
-
 def get_devices():
     target_range = "192.168.1.0/24"  # Define network range 
     devices = scan_network(target_range)  #external function
     return jsonify(devices)  
+
+@app.route('/api/device-info', methods = ["GET"])
+def device_info():
+    device = "192.168.1.105"
+    info = scan_ports(device)
+    return jsonify(info)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ monitor/port_scan.py
@@ -0,0 +1,8 @@
+import nmap
+
+def scan_ports(ip):
+    nm = nmap.PortScanner()
+    nm.scan(ip, '1-1024')  # Scan ports 1-1024
+    return nm[ip]
+
+
